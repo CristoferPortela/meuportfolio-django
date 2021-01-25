9ff77ecc757683af5ff687deaf862ea325ea85e3
Cristofer Portela <cristoferportela@outlook.com>
2021-01-25 10:13:39 -0300

    feat: model AboutMe

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,15 +1,21 @@
 from datetime import datetime
 from django.shortcuts import render
-
+from .models import AboutMe
 
 # Create your views here.
 def home(request):
     """Renders the home page."""
+
+    me = AboutMe.objects.filter(role='admin').first()
+
+    print(me)
+
     return render(
         request,
         'mainpage/index.html',
         {
             'title':'IdeiaScript',
             'year':datetime.now().year,
+            'about_me':me,
         }
     )
