a3f013285cf17a6fd1298445dfa0b63e1b3f6380
Cristofer Portela <cristoferportela@outlook.com>
2021-01-25 10:12:41 -0300

    refact: more fields added

--- mainpage/models.py
+++ mainpage/models.py
@@ -7,11 +7,25 @@ class AboutMe(models.Model):
     About me section
     """
 
-    name = models.CharField('my_name', max_length=100)
-    summary = models.CharField('summary', max_length=255)
-    about_me = models.TextField("about_me")
+    ROLES_ALLOWED = (
+        ('admin', 'Administrator'),
+        ('empl', 'Employer')
+    )
 
-    pic = models.ImageField('my_pic', width_field=150, height_field=150)
+    name = models.CharField('my name', max_length=100)
+    summary = models.CharField('summary', max_length=512)
+    about_me = models.TextField("about me")
+
+    role = models.CharField('role', choices=ROLES_ALLOWED, max_length=5)
+
+    avatar = models.ImageField(
+        'my pic', 
+        name="avatar", 
+        # width_field=150, 
+        # height_field=150, 
+        upload_to='uploads/%Y/%m/%d',
+        null=True
+    )
 
     def __str__(self):
         return self.name
