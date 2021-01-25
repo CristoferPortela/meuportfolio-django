5190dbf01c2dd19db3918a00a7545761b6dd8ca7
Cristofer Portela <cristoferportela@outlook.com>
2021-01-25 10:14:21 -0300

    feat: media paths added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,3 +118,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
 LOGIN_REDIRECT_URL = '/'
+
+MEDIA_ROOT=posixpath.join(*(BASE_DIR.split(os.path.sep) + ['uploads']))
+MEDIA_URL='/uploads/'
