f00a59ef7a72fda042d51eb6dd386363a3e956fd
Cristofer Portela <cristoferportela@outlook.com>
2021-02-03 09:58:59 -0300

    feat: registrando todos os modelos atuais no admin

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -4,3 +4,12 @@ from . import models
 
 admin.site.register(models.AboutMe)
 admin.site.register(models.Service)
+admin.site.register(models.ProjectImage)
+admin.site.register(models.Category)
+admin.site.register(models.Project)
+admin.site.register(models.ProductBenefits)
+admin.site.register(models.Product)
+admin.site.register(models.SocialMedia)
+admin.site.register(models.Slider)
+admin.site.register(models.MainPage)
+
