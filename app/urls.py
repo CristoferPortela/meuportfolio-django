c7077bb924d30f54de9577217bd54434c37221c2
Cristofer Portela <cristoferportela@outlook.com>
2021-02-13 00:42:59 -0300

    refact|feat: auth urls to app (register added)

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from app import views
+from . import forms, views
 
 urlpatterns = [
-    path('contact/', views.contact, name='contact'),
-    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='app/login.html',
+             authentication_form=forms.AuthForm,
+             extra_context={'title': 'Entrar na minha conta'}
+         ),
+        name='login'
+    ),
+    path('register', views.register, name="register"
+    ),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
 ]
